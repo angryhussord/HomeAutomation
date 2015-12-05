13ed02b0878ebc6a300b81fdec01dc725e2b7546
Patrick Hufford <pahuff@microsoft.com>
2015-12-04 16:35:54 -0800

    Adding WOL automation

---
+++ Wake-RemoteComputer.ps1
@@ -0,0 +1,10 @@
+param (
+	[string]$mac
+)
+#$Mac = "1A:2B:3C:4D:5E:6F";
+$MacByteArray = $mac -split "[:-]" | ForEach-Object { [Byte] "0x$_"};
+[Byte[]] $MagicPacket = (,0xFF * 6) + ($MacByteArray  * 16);
+$UdpClient = New-Object System.Net.Sockets.UdpClient;
+$UdpClient.Connect(([System.Net.IPAddress]::Broadcast),7);
+$UdpClient.Send($MagicPacket,$MagicPacket.Length);
+$UdpClient.Close();
